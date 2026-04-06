18e6a7436da86b9104f9644dcd75754d691eeb31
Dev <Dev.Sterling@outlook.com>
2026-04-06 19:44:11 +0530

    fixed a responsive issue

--- frontend/src/components/TeamSection.jsx
+++ frontend/src/components/TeamSection.jsx
@@ -43,7 +43,7 @@ function TeamSection() {
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
       onMouseMove={handleMove}
-      className="relative sm:hidden"
+      className="relative hidden md:block"
     >
 
       {/* TEXT CONTENT */}
